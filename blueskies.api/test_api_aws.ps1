da0d5a95e21d57255b1effd57d4cc553ce193b86
Tamlin Cowgill <tamlin.cowgill@gmail.com>
2022-12-14 06:42:15 +1100

    update ps test

--- blueskies.api/test_api_aws.ps1
+++ blueskies.api/test_api_aws.ps1
@@ -1,13 +1,14 @@
 ﻿$header = @{ "content-type" = "application/json" }
 
 $uri = "https://avr2jrozh1.execute-api.ap-southeast-2.amazonaws.com/Prod/sessions"
-$response = invoke-restmethod -Uri $uri -Method Post -Body $body -Headers $header
+$response = invoke-restmethod -Uri $uri -Method Post -Body "" -Headers $header
+Write-Host "Session output: $($response | ConvertTo-Json)"
 
-$session = $response.content | convertfrom-json
-$sessionid = $session.HostedTokenizationId
+$sessionid = $response.HostedTokenizationId
 
 $body = @{ amount = 100.00; sessionid = $sessionid } | ConvertTo-Json
 
 $uri = "https://avr2jrozh1.execute-api.ap-southeast-2.amazonaws.com/Prod/payments"
 $response = invoke-restmethod -Uri $uri -Method Post -Body $body -Headers $header
 
+Write-Host "Payment output: $($response | ConvertTo-Json)"
